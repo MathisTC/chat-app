89c00d88b61725a8ff1c060d208d145953493fa5
Mathis <mathis.thuel@gmail.com>
2023-12-04 13:50:48 +0100

    Correctifs

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,13 +1,13 @@
 <template>
-  <section class="h-screen my-auto bg-gray-50 dark:bg-gray-900">
+  <section class="h-screen my-auto  bg-gray-900">
     <div class="flex flex-col items-center justify-center px-3 py-8 mx-auto md:h-screen lg:py-0">
-      <a class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+      <a class="flex items-center mb-6 text-2xl font-semibold text-white">
         Enregistrement
       </a>
       <div
-        class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        class="w-full rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0 bg-gray-800 border-gray-700">
         <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-          <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+          <h1 class="text-xl font-bold leading-tight tracking-tight md:text-2xl text-white">
             Créez un compte
           </h1>
           <form class="space-y-4 md:space-y-6" @submit.prevent="register">
@@ -15,13 +15,14 @@
               <a class="btn" onclick="my_modal_2.showModal()">Choisir une image</a>
               <img class="rounded-full w-16 h-16" :src="image" v-if="image !== '' && fileBytes !== null" />
             </div>
+
             <dialog id="my_modal_2" class="modal">
               <div class="modal-box">
                 <h3 class="font-bold text-lg">Hello!</h3>
                 <p class="py-4">Veuillez choisir une image puis la rogner</p>
                 <input @change="onFileChange" type="file"
                   class="file-input file-input-bordered file-input-accent ghost w-full max-w-xs mb-2" />
-                <cropper ref="cropper" v-if="image != ''" :src="image" :stencil-props="{ aspectRatio: 1 / 1 }"
+                <cropper ref="cropper" v-if="image != '' && changeImg" :src="image" :stencil-props="{ aspectRatio: 1 / 1 }"
                   :stencil-component="$options.components.CircleStencil" />
                 <form method="dialog" class="modal-backdrop">
                   <button class="btn mt-5 btn-outline btn-error btn-sm btn-block">Annuler</button>
@@ -30,42 +31,42 @@
                 </form>
               </div>
             </dialog>
+
             <div>
-              <label for="nom" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nom</label>
+              <label for="nom" class="block mb-2 text-sm font-medium text-white">Nom</label>
               <input v-model="nom" name="nom" id="nom"
-                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                class="border sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
                 placeholder="Nom">
             </div>
             <div>
-              <label for="prenom" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Prenom</label>
+              <label for="prenom" class="block mb-2 text-sm font-medium text-white">Prenom</label>
               <input v-model="prenom" name="prenom" id="prenom"
-                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                class="border sm:text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
                 placeholder="Email">
             </div>
             <div>
-              <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+              <label for="email" class="block mb-2 text-sm font-medium text-white">Email</label>
               <input v-model="email" type="email" name="email" id="email"
-                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                class="border sm:text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
                 placeholder="Email">
             </div>
             <div>
-              <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Mot de
+              <label for="password" class="block mb-2 text-sm font-medium text-white">Mot de
                 passe</label>
               <input v-model="password" type="password" name="password" id="password" placeholder="••••••••"
-                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-            </div>
+              class="border sm:text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500">
+              </div>
             <div>
-              <label for="password2" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirmation du
-                mot de
-                passe</label>
+              <label for="password2" class="block mb-2 text-sm font-medium text-white">Confirmation du
+                mot de passe</label>
               <input v-model="password2" type="password" name="password2" id="password2" placeholder="••••••••"
-                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+              class="border sm:text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500">
             </div>
             <p v-if="error != ''"> {{ error }} </p>
             <button class="w-full text-center btn btn-outline btn-accent">S'enregistrer</button>
-            <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+            <p class="text-sm font-light text-gray-400">
               Vous avez déjà un compte ? <router-link :to="{ path: '/login' }"
-                class="font-bold text-primary-600 hover:underline dark:text-primary-500">Connectez vous</router-link>
+                class="font-bold hover:underline text-primary-500">Connectez vous</router-link>
             </p>
           </form>
         </div>
@@ -93,7 +94,8 @@ export default {
       email: '',
       password: '',
       password2: '',
-      error: ''
+      error: '',
+      changeImg: true
     };
   },
   methods: {
@@ -132,11 +134,13 @@ export default {
         };
         reader.readAsArrayBuffer(blob);
       }, this.image.type);
+      this.changeImg = false
     },
 
     async onFileChange(e) {
       const file = e.target.files[0];
       this.image = URL.createObjectURL(file);
+      this.changeImg = true
     }
   }
 };
