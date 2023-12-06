63a8713e0616d5a787b40c1dd0d6f5578870888c
Mathis <mathis.thuel@gmail.com>
2023-12-06 14:35:43 +0100

    Add "Ajouter un chat" button to GroupListView (generated by blackbox.ai )

--- src/views/GroupListView.vue
+++ src/views/GroupListView.vue
@@ -2,12 +2,14 @@
   <div>
     <h1 class="text-center py-4 text-2xl font-bold">Vos conversations:</h1>
 
-    <ChatPreview link="/messages" img="https://static.vecteezy.com/system/resources/previews/013/441/267/original/chat-bubble-icon-png.png" title="Général" description="Tout le monde participe" />
+    <ChatPreview link="/messages"
+      img="https://static.vecteezy.com/system/resources/previews/013/441/267/original/chat-bubble-icon-png.png"
+      title="Général" description="Tout le monde participe" />
 
-    
-    <br>
-    <button class="mx-5 btn w-[90%] btn-outline btn-info">Ajouter un chat (non fonctionnel)</button>
 
+    <div class="flex justify-center mt-2">
+      <button class="mx-5 btn w-[90%] text-center btn-outline btn-info">Ajouter un chat (non fonctionnel)</button>
+    </div>
 
   </div>
 </template>
