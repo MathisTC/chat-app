576dc0034286ecdb9c9b6289c76371ed18982e9b
Mathis <mathis.thuel@gmail.com>
2023-12-06 14:22:54 +0100

    Update image paths to use static folder (generated by blackbox.ai )

--- src/views/GroupListView.vue
+++ src/views/GroupListView.vue
@@ -2,7 +2,7 @@
   <div>
     <h1 class="text-center py-4 text-2xl font-bold">Vos conversations:</h1>
 
-    <ChatPreview link="/messages" img="/public/bubble.ico" title="Général" description="Tout le monde participe" />
+    <ChatPreview link="/messages" img="@/assets/bubble.ico" title="Général" description="Tout le monde participe" />
 
     
     <br>
